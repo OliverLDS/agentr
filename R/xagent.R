6dc8de7cfd3efaabb976e883b4d8be5fd0c2c313
Oliver <97855548+OliverLDS@users.noreply.github.com>
2025-03-10 11:13:09 +0800

    Update xagent.R

--- R/xagent.R
+++ R/xagent.R
@@ -3,3 +3,23 @@ run_xagent <- function(memory, prompt, config, max_steps = 3L) {
   final_mem <- call_llm(new_mem, config)
   final_mem
 }
+
+run_flow <- function(memory, config, tools = TOOL_COLLECTION, max_steps = 3L) {
+  step <- 1
+  while (step <= max_steps) {
+    result <- call_llm(memory, config)
+    memory <- result$memory
+    response_text <- result$output
+
+    if (response_text %in% names(tools)) {
+      tool_func <- tools[[response_text]]
+      result <- tool_func(memory)
+      memory <- result$memory
+      if (response_text == "terminate") break
+    }
+
+    step <- step + 1
+  }
+  memory
+}
+
